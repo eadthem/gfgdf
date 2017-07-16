c51845119146ddb8ddcefebcbf99e1f2bf7e4121
cor <cor@glados.site>
2017-07-15 20:31:12 -0500

    starting PHP Conversion from bash

---
+++ liveCD_src/gfgDF.php
@@ -0,0 +1,33 @@
+#!/usr/bin/php
+<?php
+
+echo "=== Geeks 4 God - United Methodist Church of the Resurrection ===";
+echo "=== DISK KILL AND TEST.  SECURE DRIVE ERASE. 06-25-2016===";
+echo "";
+echo "No warrenty given,  This may not work. And its not our fault!";
+echo "";
+echo "Network Status";
+//ifconfig
+echo "";
+echo "WARNING WARNING WARNING";
+echo "This program will erase all detected hard drives. Permanently!";
+echo "Press Y to continue. [No] ";
+//read -n1 response
+echo "";
+if ($response != "Y")
+{
+	echo "Disks not wiped. User canciled or didnt use an uppercase Y.";
+	echo "";
+	echo "type  gfgDF.bash to rerun.";
+	str_
+	st
+}
+
+
+
+
+
+
+
+
+?>
